f58556049fe1360b1edcbb1be2d3a15f2430eaa8
Harshit Khanna <khannaharshit576@gmail.com>
2026-05-15 17:57:52 +0000

    Delete files

--- config.py
+++
@@ -1,7 +0,0 @@
-class Config:
-    SECRET_KEY = 'college_management_system_secret_key_2026'
-    
-    DB_HOST = 'localhost'
-    DB_USER = 'root'                   
-    DB_PASSWORD = 'Your password'               
-    DB_NAME = 'college_management'
